8fc8623b512cf66ba3daba88bb672f1608142a8b
PC <PC@DESKTOP-6IDDDUG>
2017-02-07 15:34:35 +0100

    Creata la classe Product

---
+++ TestAzureStorage/src/TestAzureStorage/Product.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAzureStorage
+{
+    public class Product : TableEntity
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(string partitionKey, string rowKey)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = rowKey;
+        }
+
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
--- TestAzureStorage/src/TestAzureStorage/Program.cs
+++ TestAzureStorage/src/TestAzureStorage/Program.cs
@@ -8,9 +8,18 @@ namespace TestAzureStorage
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static void Main(string[] args)
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse("UseDevelopmentStorageAccount=True");
+
+            var tableClient = storageAccount.CreateCloudTableClient();    //oggetto con tutti i metodi per lavorare con il table storage
+
+            var productTable = tableClient.GetTableReference("itsproducts");  //il nome deve essere in minuscolo e senza spazi o caratteri particolari
+
+
+            //verifico se la tabella esiste
+            //altrimenti la creo
+            await productTable.CreateIfNotExistsAsync();
         }
     }
 }
